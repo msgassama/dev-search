9492c9d68bf09d43056ad9e7aa7f6877dc424981
gassdev <gassdev@gmail.com>
2021-12-02 19:47:29 +0000

    install theme

--- findDevs/settings.py
+++ findDevs/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = ''
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
