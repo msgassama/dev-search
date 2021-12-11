1fc684023a63c08968e9298b300b9295bade8f62
gassdev <gassdev@gmail.com>
2021-12-11 21:02:52 +0000

    add signals for creating profile when user is created and delete a user when its profile is deleted

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-# Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
