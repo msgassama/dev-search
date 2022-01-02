df3f26ada49c7bae7cf32fafa49cef3773c0621a
gassdev <gassdev@gmail.com>
2022-01-02 03:09:55 +0000

    styling login and register page

--- users/forms.py
+++ users/forms.py
@@ -5,4 +5,18 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
+       
+
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'input'
+            })
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
